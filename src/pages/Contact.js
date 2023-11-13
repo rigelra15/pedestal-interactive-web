8e1bc1c81f965a7af49ee07d2be830e4372f98a2
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2023-11-14 00:24:22 +0700

    fix: Update Menu Overlay

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,69 @@
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import PedestalLogo_White from '../icons/Pedestal-White.png';
+import PedestalLogo_Green from '../icons/Pedestal-Green.png'
+import './css/home.css'; // Import a CSS file for custom styles (create this file)
+
 const Contact = () => {
-    return (
-      <div className="p-4">
-        <h1 className="text-4xl font-bold">Kontak</h1>
-        <p>Jika Anda memiliki pertanyaan, jangan ragu untuk menghubungi kami.</p>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation(); // Get the current location (URL)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <div className='font-lexend-deca border-[12px] border-[#2C8E53] h-[100vh] w-[100vw] overflow-auto bg-[#CCE5D0] relative'>
+      <div className="h-[100%] w-[100%] p-[35px]">
+        <div className='flex flex-row justify-between'>
+        <img className='absolute top-0 left-0 mt-9 ml-9' src={PedestalLogo_Green} alt="Pedestal Logo" height={60} width={60} />
+
+          {/* Hamburger Icon */}
+          {!isMenuOpen && (
+            <button
+              className="absolute top-0 right-0 mt-16 mr-14"
+              onClick={toggleMenu}
+            >
+              <div className="hamburger-line bg-black mb-[5px] w-[30px] h-[3px]"></div>
+              <div className="hamburger-line bg-black mb-[5px] w-[30px] h-[3px]"></div>
+              <div className="hamburger-line bg-black w-[30px] h-[3px]"></div>
+            </button>
+          )}
+        </div>
+
+        {/* Background Overlay */}
+        <div className={`background-overlay ${isMenuOpen ? 'open' : ''}`} />
+
+        {/* Menu Overlay */}
+        <div className={`menu-overlay ${isMenuOpen ? 'open' : ''}`}>
+          {isMenuOpen && (
+            <img className='absolute top-0 left-0 mt-9 ml-9' src={PedestalLogo_White} alt="Pedestal Logo" height={60} width={60} />
+          )}
+          <div className="text-left text-white font-bold flex flex-col" style={{ marginLeft: '35px' }}>
+            {/* Apply conditional styling based on the current location */}
+            {/* Close Button (X Button) */}
+            <button
+              className="absolute top-0 right-0 mt-16 mr-14 text-white"
+              onClick={closeMenu}
+            >
+              <div className="hamburger-line rotate-45 bg-white rounded mb-[5px] w-[30px] h-[3px]"></div>
+              <div className="hamburger-line -mt-2 -rotate-45 bg-white rounded mb-[5px] w-[30px] h-[3px]"></div>
+            </button>
+            <Link to="/" className={location.pathname === '/' ? 'menu-item active' : 'menu-item'}>HOME</Link>
+            <Link to="/contact" className={location.pathname === '/contact' ? 'menu-item active' : 'menu-item'}>CONTACT</Link>
+            <Link to="/technology" className={location.pathname === '/technology' ? 'menu-item active' : 'menu-item'}>TECHNOLOGY</Link>
+            <Link to="/news" className={location.pathname === '/news' ? 'menu-item active' : 'menu-item'}>NEWS</Link>
+            <Link to="/partners" className={location.pathname === '/partners' ? 'menu-item active' : 'menu-item'}>PARTNERS</Link>
+            <Link to="/connect" className={location.pathname === '/connect' ? 'menu-item active' : 'menu-item'}>CONNECT</Link>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Contact;
-  
+    </div>
+  );
+}
+
+export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,96 +1,83 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import PedestalLogo from '../icons/pedestal.png';
+import { Link, useLocation } from 'react-router-dom';
+import PedestalLogo_White from '../icons/Pedestal-White.png';
+import PedestalLogo_Green from '../icons/Pedestal-Green.png';
+import InstagramLogo from '../icons/instagram.svg'
+import { facebook } from 'fontawesome';
+import './css/home.css'; // Import a CSS file for custom styles (create this file)
 
 const Home = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation(); // Get the current location (URL)
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <div className='font-lexend-deca border-8 border-[#2C8E53] h-[100vh] w-[100vw] overflow-auto bg-[#CCE5D0] relative'>
+    <div className='font-lexend-deca border-[12px] border-[#2C8E53] h-[100vh] w-[100vw] overflow-auto bg-[#CCE5D0] relative'>
       <div className="h-[100%] w-[100%] p-[35px]">
+        <div className='flex flex-row justify-between'>
+        {!isMenuOpen ? (
+          <img className='absolute top-0 left-0 mt-9 ml-9 z-20' src={PedestalLogo_Green} alt="Pedestal Logo" height={60} width={60} />
+        ) : (
+          <img className='absolute top-0 left-0 mt-9 ml-9 z-30' src={PedestalLogo_White} alt="Pedestal Logo" height={60} width={60} />
+        )}
 
-        <img src={PedestalLogo} alt="Pedestal Logo" height={60} width={60} />
+          {/* Hamburger Icon */}
+          {!isMenuOpen && (
+            <button
+              className="absolute top-0 right-0 mt-16 mr-14"
+              onClick={toggleMenu}
+            >
+              <div className="hamburger-line bg-black mb-[5px] w-[30px] h-[3px]"></div>
+              <div className="hamburger-line bg-black mb-[5px] w-[30px] h-[3px]"></div>
+              <div className="hamburger-line bg-black w-[30px] h-[3px]"></div>
+            </button>
+          )}
+          <facebook />
+        </div>
 
-        {/* Hamburger Icon */}
-        <button 
-          className="absolute top-0 right-0 mt-[30px] mr-[30px] hamburger" 
-          onClick={toggleMenu}
-        >
-          <div className="hamburger-line bg-black mb-[5px] w-[30px] h-[3px]"></div>
-          <div className="hamburger-line bg-black mb-[5px] w-[30px] h-[3px]"></div>
-          <div className="hamburger-line bg-black w-[30px] h-[3px]"></div>
-        </button>
+        {/* Background Overlay */}
+        <div className={`background-overlay ${isMenuOpen ? 'open' : ''}`} />
 
         {/* Menu Overlay */}
-        {isMenuOpen && (
-          <div className="absolute top-0 right-0 bottom-0 left-0 bg-black bg-opacity-75 flex justify-center items-center">
-            <div className="text-center">
-              <Link to="/" className="text-white text-xl">HOME</Link>
-              <Link to="/mission" className="text-white text-xl">MISSION</Link>
-              <Link to="/technology" className="text-white text-xl">TECHNOLOGY</Link>
-              <Link to="/news" className="text-white text-xl">NEWS</Link>
-              <Link to="/partners" className="text-white text-xl">PARTNERS</Link>
-              <Link to="/connect" className="text-white text-xl">CONNECT</Link>
-            </div>
+        <div className={`menu-overlay ${isMenuOpen ? 'open' : ''}`}>
+          {/* {isMenuOpen && (
+            <img className='absolute top-0 left-0 mt-9 ml-9' src={PedestalLogo_White} alt="Pedestal Logo" height={60} width={60} />
+          )} */}
+          <div className="text-left text-white font-bold flex flex-col" style={{ marginLeft: '35px' }}>
+            {/* Apply conditional styling based on the current location */}
+            {/* Close Button (X Button) */}
+            <button
+              className="absolute top-0 right-0 mt-16 mr-14 text-white"
+              onClick={closeMenu}
+            >
+              <div className="hamburger-line rotate-45 bg-white rounded mb-[5px] w-[30px] h-[3px]"></div>
+              <div className="hamburger-line -mt-2 -rotate-45 bg-white rounded mb-[5px] w-[30px] h-[3px]"></div>
+            </button>
+            <Link to="/" className={location.pathname === '/' ? 'menu-item active' : 'menu-item'}>HOME</Link>
+            <Link to="/contact" className={location.pathname === '/contact' ? 'menu-item active' : 'menu-item'}>CONTACT</Link>
+            <Link to="/technology" className={location.pathname === '/technology' ? 'menu-item active' : 'menu-item'}>TECHNOLOGY</Link>
+            <Link to="/news" className={location.pathname === '/news' ? 'menu-item active' : 'menu-item'}>NEWS</Link>
+            <Link to="/partners" className={location.pathname === '/partners' ? 'menu-item active' : 'menu-item'}>PARTNERS</Link>
+            <Link to="/connect" className={location.pathname === '/connect' ? 'menu-item active' : 'menu-item'}>CONNECT</Link>
           </div>
-        )}
+
+          <a href='https://www.instagram.com/pedestal_id' className='absolute bottom-0 right-0 mb-16 mr-14 flex flex-col items-end gap-2 bg-gradient-to-b from-[#77BF82] to-[#3A6742] px-4 py-2 rounded-md'>
+            {/* <p className='text-white'>Our Social Media:</p> */}
+            <div className='flex flex-row justify-center items-center gap-3 text-white'>
+              <p className='font-bold'>@pedestal.id</p>
+              <img src={InstagramLogo} alt="Instagram Logo" height={25} width={25} />
+            </div>
+          </a>
+
+        </div>
       </div>
-      <style>
-        {`
-        .hamburger {
-          display: flex;
-          flex-direction: column;
-          justify-content: space-around;
-          align-items: center;
-          background: transparent;
-          border: none;
-          cursor: pointer;
-          z-index: 20;
-        }
-        
-        .hamburger-line {
-          transition: all 0.3s ease;
-        }
-        
-        /* When the menu is open, you can add animations or transformations to the hamburger lines */
-        .hamburger-open .line1 {
-          transform: rotate(45deg) translate(5px, 5px);
-        }
-        
-        .hamburger-open .line2 {
-          opacity: 0;
-        }
-        
-        .hamburger-open .line3 {
-          transform: rotate(-45deg) translate(7px, -6px);
-        }
-        
-        /* Menu overlay */
-        .menu-overlay {
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: rgba(0, 0, 0, 0.85);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          z-index: 10;
-        }
-        
-        .menu-item {
-          color: white;
-          font-size: 1.5rem;
-          margin: 1rem;
-          text-decoration: none;
-        }        
-        `}
-      </style>
     </div>
   );
 }
